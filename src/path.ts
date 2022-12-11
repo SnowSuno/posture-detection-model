a379f1289fc7b2fd045cc4c05f12ff2575e4ba7a
Soonho Kwon <percy3368@gmail.com>
2022-12-11 22:44:40 +0900

    Update model

--- src/path.ts
+++ src/path.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 
 
 export const DATASET_PATH = path.join(__dirname, "..", "dataset");
-export const MODEL_PATH = path.join(__dirname, "..", "model");
+export const MODEL_PATH = path.join(__dirname, "..", "models");
 
 export const datasetPath = (name: string) => path.join(DATASET_PATH, name);
-export const modelPath = (name: string) => path.join(MODEL_PATH, name);
+export const modelPath = (name: string) => `file://${path.join(MODEL_PATH, name)}`;
